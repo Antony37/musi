e2107ff0545c9e0216a3c1739bf59d1d27ae3f4d
Wallvon <42699143+Wallvon@users.noreply.github.com>
2018-12-02 20:42:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,33 @@ from discord.voice_client import VoiceClient
 import asyncio
 import time
 import os
+from discord import opus
+OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+             'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
+
+def load_opus_lib(opus_libs=OPUS_LIBS):
+    if opus.is_loaded():
+        return True
+
+    for opus_lib in opus_libs:
+            try:
+                opus.load_opus(opus_lib)
+                return
+            except OSError:
+                pass
+
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(opus_libs)))
+
+
+opts = {
+    'default_search': 'auto',
+    'quiet': True,
+}  # youtube_dl options
+
+
+load_opus_lib()
 
 startup_extensions = ["Music"]
 
