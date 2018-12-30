00eb04495978c50745c53944c3ae116dceb053a8
Wallvon <42699143+Wallvon@users.noreply.github.com>
2018-12-30 18:32:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -111,53 +111,6 @@ async def hi(ctx):
 
 #hi
 
-#story
-
-@bot.command(pass_context=True)
-async def story(ctx):
-    await bot.say("Title: The Little Girl Who Wasn't")
-    await bot.say("I lived in a house from hell for four years, from age eleven to almost sixteen. There was constantly something happening. Doors flying open and shut, voices, footsteps. Nothing ever stayed where you put it. I was alone there a lot because both my parents worked and I was constantly terrified. One of the most gut-level disturbing things though was the little girl in my bathroom. Every time I walked past my bathroom door (which was constantly since it was right outside my bedroom) I saw a little girl with blond curled hair and a rose-colored dress. She just stood there, staring, looking like a photograph from 1905. I started keeping the door closed so I could walk by without seeing her, but she was always there when I opened it. Once I stepped in past her, I couldn't see her anymore but I could feel her there. She scared me, but I felt really sorry for her because she was trapped there, just like me, but probably forever. As the years went by and things in the house continued to get worse, she started seeming... darker. I started feeling like she wasn't really a little girl. I knew there was something ugly in the house and I felt like it was presenting this sympathetic image to me. Then I started thinking I was completely losing my mind.")
-    await bot.say("One day, when I was 14, I had a friend from out of town come stay with me for a week. I hadn't told her anything whatsoever about the house because I didn't think she would come if I did. Right after she got there we were sitting in my room and she left to go to the bathroom. About a minute later she walked back in with a puzzled look on her face and said So, there's a little girl in your bathroom. Um, I, yeah she hangs out in there. Blond hair?" "Curls? Pink dress? Yeah. You know that's not really a little girl, don't you? I almost threw up. I was so relieved and terrified and excited and ready to run out of the house screaming. She wouldn't use my bathroom the rest of the week and I started using it as little as possible without pissing off my parents (who did not want to believe). Eventually we moved out and I could not have been happier. I distanced myself from it mentally as much as I could. Then, when I was 18, I took another friend on a road trip to pack up a few things I'd left in the house (my parents hadn't managed to sell it, and wouldn't for 5 more years). The minute we got on the property, my friend seemed uncomfortable. When we came around the bend in the long, steep driveway, he went completely white. I could tell something was wrong, but he insisted he was OK, so we got to work. After a while he asked to use the bathroom and I directed him to mine. Not 20 seconds after he left, he came running back in, gasping for breath, andand slammed the bedroom door behind him. He started babbling about a little blond girl who isn't really a little girl. All of a sudden he went dead still, looked me in the eye, and very solemnly said She's not happy. With you. You left, and you weren't supposed to. We threw whatever we could grab in two trips in my car (after I walked him to another bathroom and waited outside the door) and got the fuck out at top speed.")
-
-#story
-
-#kill
-
-@bot.command(pass_context=True)
-async def kill(ctx):
-    await bot.say("Egroid: You're gonna die! :gun:")
-    await bot.say("You: AHHHHHHHHH!")
-    await bot.say("Egroid Points the gun at you")
-    await bot.say("Gun: *click*")
-    await bot.say("Egroid: Wait, Why did nothing happen??")
-    await bot.say("You: I'm gonna call the Police! :iphone:")
-    await bot.say("Egroid: Where is my ammo?")
-    await bot.say("You: Picks up phone and calls **911**")
-    await bot.say("Police: Police how can i help you?")
-    await bot.say("You: There is a man pointing a gun at me!!")
-    await bot.say("Police: We are coming to your exact location!")
-    await bot.say("Egroid: Ahhhh Here is my ammo!")
-    await bot.say("Egroid hears the sirens from the Police car :police_car: :oncoming_police_car:")
-    await bot.say("Gun: *Click* Egroid: Reloaded!")
-    await bot.say("FBI: **FBI** DROP THE GUN NOW!")
-    await bot.say("Gun: **Pow!**")
-    await bot.say("*You fell dead* :gravestone:")
-    await bot.say("FBI'S Gun: **Pow!**")
-    await bot.say("Egroid fell dead :gravestone:")
-    print ("The Kill command is used")
-
-#kill
-
-#changelanguage
-
-@bot.command(pass_context=True)
-async def changelanguage(ctx):
-    await bot.say(":flag_nl: :flag_us: You want to change the language? :flag_gb: :flag_jp:")
-    await bot.say("I'm sorry to say that you can't do that in this version of the bot")
-    await bot.say("Maybe in some next version but idk")
-
-#changelanguage
-
 #info
 
 @bot.command(pass_context=True)
@@ -199,7 +152,7 @@ async def clear(ctx, amount=100):
 
 @bot.command(pass_context=True)
 async def botinfo(ctx):
-    embed = discord.Embed(title="Version", description="I'm now on version 1.83", color=0x00ff00)
+    embed = discord.Embed(title="Version", description="I'm now on version 1.84", color=0x00ff00)
     embed.set_footer(text="Made with ❤️ by Wallvon")
     embed.set_author(name="Egroid")
     embed.add_field(name="Where is the source code?", value="https://github.com/Wallvon/egroidbot", inline=True)
