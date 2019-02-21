b77e55ae583b0199f6f48dda00c832a18dbdf336
Wallvon <42699143+Wallvon@users.noreply.github.com>
2019-02-21 11:27:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 from discord.utils import find
 from discord.voice_client import VoiceClient
-import json
 import asyncio
 import time
 import os
@@ -46,7 +45,7 @@ bot = commands.Bot(command_prefix='%')
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(game=discord.Game(name='{} Servers | %help'.len(self.bot.servers)))
+    await bot.change_presence(game=discord.Game(name='%help | %invite'))
     print ("I'm Ready, Fresh and Started!")
     print ("I am running on " + bot.user.name)
     print ("With the ID: " + bot.user.id)
@@ -57,45 +56,6 @@ async def on_member_join(member):
     await bot.send_message(member, "Welcome to the server! I hope you have a great time!")
     print("Sent message to " + member.name)
 
-@bot.event
-async def on_member_join(member):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-
-        await update_data(users, member)
-
-        with open('users.json', 'w') as f:
-            json.dump(users, f)
-
-@bot.event
-async def on_message(message):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
-
-        await update_data(users, message.author)
-        await add_experience(users, message.author, 5)
-        await level_up(users, message.author, message.channel)
-
-        with open('users.json', 'w') as f:
-            json.dump(users, f)
-
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]['experience'] = 0
-        users[user.id]['level'] = 0
-
- async def add_experience(users, user, exp):
-     users[user.id]['experience'] += exp
-
-     async def level_up(users, user, channel):
-         experience = users[user.id]['experience']
-         lvl_start = users[user.id]['level']
-         lvl_end = int(experience ** (1/4))
-
-         if lvl_start < lvl_end:
-             await bot.send_message(channel, '{} has leveled up to level {}'.format(user.mention, lvl_end))
-             users[user.id]['level'] = lvl_end
 
 class Main_Commands():
     def __init__(self, bot):
